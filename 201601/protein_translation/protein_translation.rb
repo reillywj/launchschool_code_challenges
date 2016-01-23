debf67882be9e782b0995ea40e122e9eb55fc94d
William Reilly <William.Reilly2010@gmail.com>
2016-01-22 19:35:14 -0500

    completed and passed all tests; in adding InvalidCodonError I had to adjust test_suite to equal Translation::InvalidCodonError

---
+++ 201601/protein_translation/protein_translation.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Translation
+  POLY = {methionine: %w(AUG), phenylalanine: %w(UUU UUC), leucine: %w(UUA UUG ), serine: %w(UCU UCC UCA UCG), tyrosine: %w(UAU UAC), cysteine: %w(UGU UGC), tryptophan: %w(UGG), stop: %w(UAA UAG UGA)}
+  
+  class InvalidCodonError < StandardError
+    def initialize
+    end
+  end
+
+  def self.of_codon(rna)
+    values = POLY.select{ |_,value| value.include?(rna)}.keys
+    if values.empty?
+      fail InvalidCodonError
+    else
+      value = values.first.to_s
+      value == 'stop' ? value.upcase : value.capitalize
+    end
+  end
+
+  def self.of_rna(strand)
+    arr = []
+    until strand.length <= 0
+      codon = self.of_codon strand.slice!(0,3)
+      codon == 'STOP' ? break : arr << codon
+    end
+    arr
+  end
+end
