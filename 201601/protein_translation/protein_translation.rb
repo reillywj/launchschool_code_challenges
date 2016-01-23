b53c75c5443ece8436f6bf46c6875ac45ec61df2
William Reilly <William.Reilly2010@gmail.com>
2016-01-22 19:40:53 -0500

    refactored for rubocop to no offenses detected

--- 201601/protein_translation/protein_translation.rb
+++ 201601/protein_translation/protein_translation.rb
@@ -1,15 +1,24 @@
 require 'pry'
 
+# Translation translates strands of codons from letter-form to polypeptide names
 class Translation
-  POLY = {methionine: %w(AUG), phenylalanine: %w(UUU UUC), leucine: %w(UUA UUG ), serine: %w(UCU UCC UCA UCG), tyrosine: %w(UAU UAC), cysteine: %w(UGU UGC), tryptophan: %w(UGG), stop: %w(UAA UAG UGA)}
-  
+  POLY = { methionine: %w(AUG),
+           phenylalanine: %w(UUU UUC),
+           leucine: %w(UUA UUG ),
+           serine: %w(UCU UCC UCA UCG),
+           tyrosine: %w(UAU UAC),
+           cysteine: %w(UGU UGC),
+           tryptophan: %w(UGG),
+           stop: %w(UAA UAG UGA) }
+
+  # Translation::InvalidCodonError for use when codon doesn't exist in POLY
   class InvalidCodonError < StandardError
     def initialize
     end
   end
 
   def self.of_codon(rna)
-    values = POLY.select{ |_,value| value.include?(rna)}.keys
+    values = POLY.select { |_, value| value.include?(rna) }.keys
     if values.empty?
       fail InvalidCodonError
     else
@@ -21,7 +30,7 @@ class Translation
   def self.of_rna(strand)
     arr = []
     until strand.length <= 0
-      codon = self.of_codon strand.slice!(0,3)
+      codon = of_codon strand.slice!(0, 3)
       codon == 'STOP' ? break : arr << codon
     end
     arr
