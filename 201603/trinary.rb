11a2c601e93f3c65a686ee02ea807afbf79fdd07
Will Reilly <william.reilly2010@gmail.com>
2016-03-14 18:33:34 +0000

    rubocop refactor; minor changes

--- 201603/trinary.rb
+++ 201603/trinary.rb
@@ -1,8 +1,9 @@
+# Trinary converts a trinary number to a decimal (base 10)
 class Trinary
   def initialize(input)
     @value = input.to_i.to_s
   end
-  
+
   def to_decimal
     decimal = 0
     @value.reverse.split('').each_with_index do |num, index|
