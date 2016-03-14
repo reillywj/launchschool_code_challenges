1de18fbad26357dc5621d30b0dfb66284c2c7e05
Will Reilly <william.reilly2010@gmail.com>
2016-03-14 18:32:13 +0000

    Complete ternary challenge.

---
+++ 201603/trinary.rb
@@ -0,0 +1,13 @@
+class Trinary
+  def initialize(input)
+    @value = input.to_i.to_s
+  end
+  
+  def to_decimal
+    decimal = 0
+    @value.reverse.split('').each_with_index do |num, index|
+      decimal += num.to_i * 3**index
+    end
+    decimal
+  end
+end
