c42e3dba1a585d0db1182a51a580fae9a86e9b26
William Reilly <William.Reilly2010@gmail.com>
2016-04-15 22:47:46 -0400

    Refactored to remove duplicity of breakdowns.

--- 201604/largest_product/largest_sub_product.rb
+++ 201604/largest_product/largest_sub_product.rb
@@ -1,53 +1,65 @@
-require 'pry'
+# LaunchSchool Challenge
+# From Project Euler problem #8
+# Find the thirteen(13) adjacent digits in the 1000-digit number that have the greatest product.
+# What is the value of this product?
 
+# Breakdown is common code for finding the simple counts of values within an array or collection.
+module Breakdown
+  attr_reader :breakdown
+
+  def find_breakdown(arr = nil)
+    @breakdown = {}
+    breakdown_proc = proc do |val|
+      @breakdown[val] = 0 unless @breakdown[val]
+      @breakdown[val] += 1
+    end
+    if block_given?
+      yield.each { |val| breakdown_proc.call(val) }
+    else
+      arr.each { |val| breakdown_proc.call(val) }
+    end
+  end
+end
+
+# SubProductFinder: Finds the sub products
 class SubProductFinder
   attr_reader :number
+
+  include Breakdown
+
   def initialize(filename)
-    @number = IO.readlines(filename).join('').gsub("\n", '')
+    @number = IO.readlines(filename).join('').delete("\n")
   end
 
-  def find_largest_product_subset(n=13)
+  def find_largest_product_subset(n = 13)
     find_subsets(n) unless @subsets
     @subsets.sort[-1]
   end
 
-  def show_breakdown(num=nil)
-    proc_breakdown = Proc.new do |pair|
-      "[#{pair.first}: #{'*' * pair[-1]}]\n"
-    end
-
+  def show_breakdown(num = nil)
     output = ''
 
-    if num >= 0
-      breakdown[0, num].each do |pair|
-        output << proc_breakdown.call(pair)
-      end
-    elsif num < 0
-      breakdown[num, num.abs].reverse.each do |pair|
-        output << proc_breakdown.call(pair)
-      end
+    proc_breakdown = proc { |pair| output << "[#{pair.first}: #{'*' * pair[-1]}]\n" }
+
+    case num
+    when -breakdown.size..-1
+      breakdown[num, num.abs].reverse_each { |pair| proc_breakdown.call(pair) }
     else
-      breakdown.each do |pair|
-        output << proc_breakdown.call(pair)
-      end
+      breakdown[0, num || breakdown.size].each { |pair| proc_breakdown.call(pair) }
     end
-    puts output
+
+    output
   end
 
   def breakdown
     return @breakdown if @breakdown
-    @breakdown = {}
-    @subsets.each do |subset|
-      product = subset.product
-      @breakdown[product] = 0 unless @breakdown[product]
-      @breakdown[product] += 1
-    end
+    find_breakdown(@subsets.map(&:product))
     @breakdown = @breakdown.to_a.sort { |a, b| a.first <=> b.first }
   end
 
   private
 
-  def find_subsets(n=13)
+  def find_subsets(n = 13)
     return @subsets if @subsets
 
     @subsets = []
@@ -57,16 +69,24 @@ class SubProductFinder
   end
 end
 
+# Subset takes a subset of digits and parses them into digit statistics
+# Also finds and stores the product of these digits
 class Subset
   attr_reader :number
 
+  include Breakdown
+
   def initialize(subset)
     @number = subset
-    @breakdown = {}
-    subset.split('').each do |num|
-      @breakdown[num] = 0 unless @breakdown[num]
-      @breakdown[num] += 1
+    find_breakdown do
+      subset.split('').map(&:to_i)
     end
+    # subset.split('').each do |num|
+    #   num = num.to_i
+    #   @breakdown[num] = 0 unless @breakdown[num]
+    #   @breakdown[num] += 1
+    # end
+    @breakdown = @breakdown.to_a.sort { |a, b| a.first <=> b.first }.to_h
   end
 
   def product
@@ -74,25 +94,35 @@ class Subset
     return 0 if @breakdown['0']
     value = 1
     @breakdown.each do |number, count|
-      value *= (number.to_i**count.to_i)
+      value *= (number**count)
     end
     @product = value
   end
 
   def <=>(other)
-    self.product <=> other.product
+    product <=> other.product
   end
 
   def to_s
-    "#{self.number} => #{self.product}"
+    "Digits: #{number}\nBreakdown: #{breakdown}\nProduct: #{product}"
   end
+end
+
+# Running the Solution
+
+def title(words)
+  puts words.center(50, '-')
 end
 
 problem = SubProductFinder.new('number.txt')
 solution = problem.find_largest_product_subset(13)
-puts "Problem: Find largest subset of a thousand digit number in number.txt file."
+puts "\nProblem: Find largest subset of a thousand digit number in number.txt file."
+title 'Solution'
 puts solution # 5576689664895 => 23514624000
-puts "Top 10"
-problem.show_breakdown(-10)
-puts "Bottom 10"
-problem.show_breakdown(10)
+# Metrics
+title 'Top 10'
+puts problem.show_breakdown(-10)
+title 'Bottom 10'
+puts problem.show_breakdown(10)
+# title 'All'
+# puts problem.show_breakdown #Shows all possible subset products
