f796f545ba9f2ddb2360250aaa737df73ac989d3
William Reilly <William.Reilly2010@gmail.com>
2016-04-15 22:09:54 -0400

    With some analytics.

--- 201604/largest_product/largest_sub_product.rb
+++ 201604/largest_product/largest_sub_product.rb
@@ -1,10 +1,51 @@
 require 'pry'
 
 class Product
-  attr_reader :subsets
+  attr_reader :number
   def initialize(filename)
     @number = IO.readlines(filename).join('').gsub("\n", '')
   end
+
+  def find_largest_product_subset(n=13)
+    find_subsets(n) unless @subsets
+    @subsets.sort[-1]
+  end
+
+  def show_breakdown(num=nil)
+    proc_breakdown = Proc.new do |pair|
+      "[#{pair.first}: #{'*' * pair[-1]}]\n"
+    end
+
+    output = ''
+
+    if num >= 0
+      breakdown[0, num].each do |pair|
+        output << proc_breakdown.call(pair)
+      end
+    elsif num < 0
+      breakdown[num, num.abs].reverse.each do |pair|
+        output << proc_breakdown.call(pair)
+      end
+    else
+      breakdown.each do |pair|
+        output << proc_breakdown.call(pair)
+      end
+    end
+    puts output
+  end
+
+  def breakdown
+    return @breakdown if @breakdown
+    @breakdown = {}
+    @subsets.each do |subset|
+      product = subset.product
+      @breakdown[product] = 0 unless @breakdown[product]
+      @breakdown[product] += 1
+    end
+    @breakdown = @breakdown.to_a.sort { |a, b| a.first <=> b.first }
+  end
+
+  private
 
   def find_subsets(n=13)
     return @subsets if @subsets
@@ -14,28 +55,25 @@ class Product
       @subsets << Subset.new(@number[index, n])
     end
   end
-
-  def find_largest_product_subset(n=13)
-    find_subsets(n) unless @subsets
-    @subsets.sort[-1]
-  end
 end
 
 class Subset
+  attr_reader :number
+
   def initialize(subset)
-    @subset = subset
-    @numbers = {}
+    @number = subset
+    @breakdown = {}
     subset.split('').each do |num|
-      @numbers[num] = 0 unless @numbers[num]
-      @numbers[num] += 1
+      @breakdown[num] = 0 unless @breakdown[num]
+      @breakdown[num] += 1
     end
   end
 
   def product
     return @product if @product
-    return 0 if @numbers['0']
+    return 0 if @breakdown['0']
     value = 1
-    @numbers.each do |number, count|
+    @breakdown.each do |number, count|
       value *= (number.to_i**count.to_i)
     end
     @product = value
@@ -44,6 +82,17 @@ class Subset
   def <=>(other)
     self.product <=> other.product
   end
+
+  def to_s
+    "#{self.number} => #{self.product}"
+  end
 end
 
-binding.pry
+problem = Product.new('number.txt')
+solution = problem.find_largest_product_subset(13)
+puts "Problem: Find largest subset of a thousand digit number in number.txt file."
+puts solution # 5576689664895 => 23514624000
+puts "Top 10"
+problem.show_breakdown(-10)
+puts "Bottom 10"
+problem.show_breakdown(10)
