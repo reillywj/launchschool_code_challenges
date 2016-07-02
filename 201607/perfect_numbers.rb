5452b6aa32be49d3ebfa3cec40ed0cc43ba4645b
William Reilly <William.Reilly2010@gmail.com>
2016-07-02 08:42:22 -0400

    Completed PerfectNumbers.

---
+++ 201607/perfect_numbers.rb
@@ -0,0 +1,29 @@
+class PerfectNumber
+  def self.classify(num)
+    raise RuntimeError if num < 0
+    case sum_of_factors(num)
+    when num
+      "perfect"
+    when 0...num
+      "deficient"
+    else
+      "abundant"
+    end
+  end
+
+  private
+
+  def self.divisors(num)
+    divs = []
+
+    (1..Integer(Math.sqrt(num))).each do |div|
+      (divs << div) << (num / div) if num % div == 0
+    end
+
+    return divs.sort
+  end
+
+  def self.sum_of_factors(num)
+    divisors(num).inject(0, &:+) - num
+  end
+end
