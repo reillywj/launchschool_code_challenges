b1d920a099a2ddb9605369f530db10ab6a7f8b26
William Reilly <William.Reilly2010@gmail.com>
2016-07-23 21:06:27 -0400

    Refactored to slim down and meet 99% of rubocop.

--- 201607/bowling/bowling.rb
+++ 201607/bowling/bowling.rb
@@ -1,72 +1,80 @@
-require 'pry'
-
+# Game of bowling.
 class Game
   attr_accessor :score
 
   def initialize
-    @frame = Frame.new
-    @roll = 0
-    @score = 0
+    @frame           = Frame.new
+    @roll            = 0
+    @score           = 0
     @roll_multiplier = [1]
   end
 
   def roll(pins)
-    raise "Should not be able to roll after game is over" if gameover?
-    raise "Pins must have a values from 0 to 10." if !(0..10).include? pins
-    @score += @roll_multiplier[@roll] * pins
-    @frame.roll(pins)
+    raise 'Should not be able to roll after game is over' if gameover?
+    tally_score pins
+    @frame.roll pins
     set_multipliers
-    multiplier
-    # puts self
-    # puts "Pins: #{pins}"
     @frame.next_frame if @frame.over?
     @roll += 1
   end
 
-  def to_s
-    "Roll: #{@roll}; Score: #{@score}; Multiplier: #{@roll_multiplier}"
+  def tally_score(pins)
+    raise 'Pins must have a values from 0 to 10.' unless (0..10).cover? pins
+    @score += @roll_multiplier[@roll] * pins
   end
 
-  def multiplier
-    return if @frame.number >=10
+  def set_multipliers
+    initialize_multipliers
+    return if @frame.number >= 10
     @roll_multiplier[@roll + 1] += 1 if @frame.ten?
     @roll_multiplier[@roll + 2] += 1 if @frame.strike?
   end
 
-  def set_multipliers
-    @roll_multiplier[@roll + 1] ||= 1
-    @roll_multiplier[@roll + 2] ||= 1
+  def initialize_multipliers
+    2.times { |num| @roll_multiplier[@roll + num + 1] ||= 1 }
   end
 
   def score
-    raise "Score cannot be taken until the end of the game" unless gameover?
+    raise 'Score cannot be taken until the end of the game' unless gameover?
     @score
   end
 
   def gameover?
-    @frame.number > 10
+    @frame > 10
   end
 end
 
+# A frame in bowling. This moves along from frame to frame.
 class Frame
   attr_reader :number
 
-  def initialize(number=1, rolls = nil)
-    @number = 1
-    @rolls = rolls || []
+  def initialize
+    @number    = 1
+    @rolls     = []
     @available = 10
   end
 
+  def >(number)
+    @number > number
+  end
+
   def roll(pins)
-    @available -= pins
-    raise "Pin count exceeds pins on the lane" if @available < 0
+    decrement pins
     @rolls << pins
-    # binding.pry if @number == 10
+    reset_pins
+  end
+
+  def decrement(pins)
+    @available -= pins
+    raise 'Pin count exceeds pins on the lane' if @available < 0
+  end
+
+  def reset_pins
     @available = 10 if ten? || max_rolls?
   end
 
   def ten?
-    pins_down == 10 || @rolls.last == 10
+    pins_down == 10 || strike?
   end
 
   def pins_down
@@ -74,15 +82,11 @@ class Frame
   end
 
   def strike?
-    ten? && @rolls.size == 1
+    @rolls.last == 10
   end
 
   def max_rolls
-    if @number == 10 && pins_down >= 10
-      3
-    else
-      2
-    end
+    (@number == 10 && pins_down >= 10) ? 3 : 2
   end
 
   def max_rolls?
