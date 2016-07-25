07c44324aa07eba7b0ed9709281c9ef4e3792d1e
William Reilly <William.Reilly2010@gmail.com>
2016-07-24 20:35:46 -0400

    Initial refactoring ocr numbers.

--- 201607/ocr_numbers/ocr.rb
+++ 201607/ocr_numbers/ocr.rb
@@ -1,29 +1,35 @@
-require 'pry'
-
 # OCR Number Challenge
 class OCR
-  # attr_reader :text
-
   def initialize text
-    @rows = text.split("\n\n")
-    @rows.map! { |row| row.split("\n") }
-    @rows.each do |row|
-      row.map! { |text| (text.length % 3 == 0 && text.length > 0)? text : text + ' ' * (3 - text.length % 3) }
-    end
-
-    @digit_rows = []
-    @rows.each_with_index do |row, index|
-      @digit_rows[index] = Array.new(row.first.length / 3) { Digit.new }
-    end
-
-    # @text = text.split "\n"
-    # @text.map! { |text| text.length % 3 == 0 ? text : text + ' ' * (3 - text.length % 3) }
-    # @digits = Array.new(@text.first.length / 3) { Digit.new}
+    digest text
+    digitize
   end
 
   def convert
     read_text_by_row
     translate_digits
+  end
+
+  # Private methods
+  def digest text
+    @rows = text.split("\n\n")
+    @rows.map! { |row| row.split("\n") }
+    @rows.each do |row|
+      row.map! do |text|
+        correct_characters?(text) ? text : text + ' ' * (3 - text.length % 3)
+      end
+    end
+  end
+
+  def digitize
+    @digit_rows = []
+    @rows.each_with_index do |row, index|
+      @digit_rows[index] = Array.new(row.first.length / 3) { Digit.new }
+    end
+  end
+
+  def correct_characters? text
+    text.length % 3 == 0 && text.length > 0
   end
 
   def read_text_by_row
@@ -34,28 +40,19 @@ class OCR
         end
       end
     end
-    # text.each_with_index do |row, row_index|
-    #   @digits.each_with_index do |digit, digit_index|
-    #     digit.add row_index, row[digit_range digit_index]
-    #   end
-    # end
   end
 
   def digit_range digit_index
-    # 0 -> 0..2
-    # 1 -> 3..5
-    # 2 -> 6..8
     start = digit_index * 3
     ending = start + 2
     start..ending
   end
 
   def translate_digits
-    # @digits.reduce('') { |number, next_digit| number + next_digit.to_s }
     @digit_rows.map { |digit_row| digit_row.reduce ('') { |number, next_digit| number + next_digit.to_s }}.join(',')
   end
 
-  private :read_text_by_row, :translate_digits
+  private :read_text_by_row, :translate_digits, :digit_range, :correct_characters?, :digest, :digitize
 end
 
 class Digit
