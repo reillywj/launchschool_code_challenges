7d56d42f00839500adcd7b118c5a90b3d68f5a76
William Reilly <William.Reilly2010@gmail.com>
2016-08-20 01:01:10 -0400

    Refactored.

--- 201608/rail_fence/rail_fence_cipher.rb
+++ 201608/rail_fence/rail_fence_cipher.rb
@@ -1,35 +1,33 @@
-require 'pry'
-
+# RailFenceCipher for LaunchSchool Ruby Exercise
 class RailFenceCipher
   def self.encode(str, cipher_size)
-    create_encoder(str.split(""), cipher_size).map(&:join).join
+    create_encoder(str.split(''), cipher_size).map(&:join).join
   end
 
   def self.decode(str, cipher_size)
-    mapper = create_encoder((0..str.size).to_a, cipher_size).flatten
+    decoder = create_encoder((0...str.size).to_a, cipher_size).flatten
     answer = []
-    str.split("").each_with_index do |letter, i|
-      answer[mapper[i]] = letter
+    str.split('').each_with_index do |letter, i|
+      answer[decoder[i]] = letter
     end
-    answer.join("")
+    answer.join
   end
 
   def self.create_encoder(arr, cipher_size)
     row = 0
     up = true
-    encoder = Array.new(cipher_size) {Array.new}
+    encoder = Array.new(cipher_size) { [] }
 
     arr.each_with_index do |element, i|
       encoder[row][i] = element
-      case row
-      when cipher_size - 1
+      if row == cipher_size - 1
         up = false
-      when 0
+      elsif row == 0
         up = true
       end
+
       row += up ? 1 : -1 if cipher_size > 1
     end
-    # binding.pry
     encoder.map(&:compact)
   end
 end
